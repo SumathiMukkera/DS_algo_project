987c1fa1713e2e97de8543e9ff368c5bdf026cb0
subhadra sahu <subhadrasahu12@gmail.com>
2025-01-16 23:13:44 -0500

    Commiting hooks

--- src/test/java/com/hooks/Hooks.java
+++ src/test/java/com/hooks/Hooks.java
@@ -1,10 +1,33 @@
 package com.hooks;
 
+import java.io.IOException;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+
+import com.utilities.configFileReader;
+import com.webdrivermanager.*;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
 public class Hooks {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private WebDriver driver;
+	public Properties configProp;
+	private DriverManager drivermanager = new DriverManager();
+	
+	@Before()
+	public void setUp() throws IOException {
+		configProp = configFileReader.initializeProperties();
+		driver=drivermanager.getDriver();
+		driver.get(configProp.getProperty("url"));
+		driver.manage().window().maximize();
+	}
+	
+	@After()
+	public void quitBrowser() {
+		DriverManager.getDriver().quit();
+	}
 	}
 
-}
+
