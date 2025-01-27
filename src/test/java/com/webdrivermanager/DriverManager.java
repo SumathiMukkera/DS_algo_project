5e8582a156188a7b60e0e22f5e6df0f506f1e09a
SumathiMukkera <sumathi.barupati9@gmail.com>
2025-01-27 16:52:47 -0600

    Update DriverManager.java

--- src/test/java/com/webdrivermanager/DriverManager.java
+++ src/test/java/com/webdrivermanager/DriverManager.java
@@ -1,16 +1,59 @@
-package com.webdrivermanager;
+package com.DriverFactory;
 
-import org.openqa.selenium.chrome.ChromeOptions;
+import java.io.IOException;
+import java.util.Properties;
 
-public class DriverManager {
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
-	private static void browserCapabilities() throws InterruptedException{
-	 //EdgeOptions
-	//FirefoxOptions
-    //SafariOptions
+import com.utilities.*;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class DriverFactory {
+	public WebDriver driver;
+	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+	/*This method is used to initialize the thread local driver on the basis of given browser
+	 * this will reurn tldriver
+	 */
+	public WebDriver init_driver(String browser) {
+		System.out.println("browser value is :" + browser);
+		if(browser.equals("chrome")) {
+			 WebDriverManager.chromedriver().setup();
+			 tlDriver.set(new ChromeDriver());
+		}
 		
+		else if(browser.equals("firefox")) {
+			 WebDriverManager.firefoxdriver().setup();
+			 tlDriver.set(new FirefoxDriver());
+		}
 		
-		ChromeOptions chromeOptions = new ChromeOptions();
+		else if(browser.equals("safari")) {
+			 WebDriverManager.safaridriver().setup();
+			 tlDriver.set(new SafariDriver());
+		}
+		
+		else if(browser.equals("edge")) {
+			 WebDriverManager.edgedriver().setup();
+			 tlDriver.set(new EdgeDriver());
+		}
+		
+		else {
+			System.out.println("Please pass the correct browser");
+		}
+		
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().maximize();
+		return getDriver();
+	}
 	
+	public static synchronized WebDriver getDriver() {
+		return tlDriver.get();
+		
+	}
 }
-}
+
+
