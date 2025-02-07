811c4925e4e9439665e5526fcae4bbbcade8b63b
SumathiMukkera <sumathi.barupati9@gmail.com>
2025-02-07 09:06:31 -0600

    login updates

--- src/main/java/pageobject/LoginPage.java
+++ src/main/java/pageobject/LoginPage.java
@@ -1,22 +1,30 @@
 package pageobject;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
 
 public class LoginPage {
 
 	public WebDriver driver;
 	
-	//locators
-	By GetStartedbutton = By.xpath("//button[@class='btn']");
+
+	By GetStartedbutton = By.xpath("//a[@href='/home']");
+	//button[@class='btn']"//
 	 By SignIn = By.xpath("//a[@href='/login']");
 	 By Register_onlogin = By.xpath("//a[@href='/register']");
 	By Username_field = By.xpath("//input[@name='username']");
     By Password_field = By.xpath("//input[@name='password']");
 	By Login_submit = By.xpath("//input[@value='Login']");
-	By errorMessage=By.xpath("//div[@class='alert alert-primary' and contains(text(), 'Invalid Username and Password')]");
-    By Youareloggedin = By.xpath("//div[contains(@class, 'alert') and contains(@class, 'alert-primary')]");
+	By errorMessage=By.xpath("//div[@class='alert alert-primary']");
+	By Youareloggedin = By.xpath("//div[@class='alert alert-primary']");
 	//By Register_onlogin = By.xpath("//a[@href='/register']");
 	
 	public LoginPage(WebDriver driver) {
@@ -38,14 +46,18 @@ public class LoginPage {
 		}
 	 
 	 public void navigatetoLoginpage() {
-	 
+	 //try {
 		driver.findElement(SignIn);
+	//} catch (Exception e) {
+		//System.out.println("Error occurred: " + e.getMessage());
+		//e.printStackTrace();
 	}
 			
 		
 		public String getLoginpageTitle() {
-			
-			return driver.getTitle();
+			driver.findElement(Login_submit);
+			String getLoginpageTitle = driver.getTitle();
+			return getLoginpageTitle;
 		}
 		
 	
@@ -53,25 +65,24 @@ public class LoginPage {
 		     
 				driver.findElement(Username_field).sendKeys(username);
 		    }
-        
-          
+
 		    public void setPassword(String password) {
-		        driver.findElement(Password_field).sendKeys(password);
+		        driver.findElement(getPassword_field()).sendKeys(password);
 		    }
 		    
-		    /*public By getPassword_field() {
-				driver.findElement(Password_field);
-					return Password_field;
-				}
-		     public By getUsername_field() {
-		    	 driver.findElement(Username_field);
-		    	 return Username_field;
-		     }*/
-		    
-		  
-
-			public String getErrorMessage() { //error message display
+		    public String getErrorMessage() { //error message display
 		        return driver.findElement(errorMessage).getText();
+		    }
+		    
+		    public boolean getalertforemptyusernamefield() {
+		    	WebElement usermsg = driver.findElement(Username_field);
+				boolean isrequired = usermsg.getDomAttribute("required") != null;
+				return isrequired;
+		    }
+		    public boolean getalertforemptypasswordfield() {
+		    	WebElement usermsg = driver.findElement(Password_field);
+				boolean isrequired = usermsg.getDomAttribute("required") != null;
+				return isrequired;
 		    }
 
 		    public void clickLoginButton() { //complete login page action and return homepage
@@ -96,30 +107,45 @@ public class LoginPage {
 		    
 		    public boolean isOnLoginPage() {
 		       return driver.findElement(Login_submit).isDisplayed();
-			} 
-		    
-		   
+			}
 		 
 		    public Homepage login(String username, String password) {
 		        setUsername(username);
 		        setPassword(password);
 		        clickLoginButton();
-		     //driver.findElement(Username_field).sendKeys(username);
-		    // driver.findElement(Password_field).sendKeys(password);
-		     //driver.findElement(SignIn).click();
+		     
 
 		        return new Homepage(driver);  // Assuming successful login navigates to HomePage
 		    }
 
+		    public void loginfunction(String username, String password) {
+		    	
+		    	driver.findElement(Username_field).sendKeys(username);
+		    	driver.findElement(Password_field).sendKeys(password);
+		    	
+		    }
+			public By getPassword_field() {
+				return Password_field;
+			}
+
+			public void setPassword_field(By password_field) {
+				Password_field = password_field;
+			}
+
 			public  String getYouareloggedin() {
-			return driver.findElement(Youareloggedin).getText();
+			return	driver.findElement(Youareloggedin).getText();
+	
+			}
+	
+				
 	  
 			}
-}
+
 			
 			
 
 		
 		   
 	
+
 
