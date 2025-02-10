cb821ab0593d232cea34bf555ff0fd4fce3be265
SumathiMukkera <sumathi.barupati9@gmail.com>
2025-02-10 14:26:05 -0600

    Update TestRunner.java

--- src/test/java/com/testrunner/TestRunner.java
+++ src/test/java/com/testrunner/TestRunner.java
@@ -36,7 +36,7 @@ public class TestRunner extends AbstractTestNGCucumberTests {
 	}
 	
 	@Override
-	@DataProvider(parallel = true)    
+	@DataProvider(parallel = false)    
 	 public Object[][] scenarios() 
 	{
 	   return super.scenarios(); 
